6bee1d6172c3ce3be2cc726912d5933448ee2e07
AlessandroD98 <alessandro.comunicazioni@gmali.com>
2023-01-18 16:56:13 +0100

    com

---
+++ index.js
@@ -0,0 +1,22 @@
+let imgObj = null;
+
+const button = document.getElementById("button1");
+button.addEventListener("click", moveRight);
+
+function init() {
+  imgObj = document.getElementById("img");
+  imgObj.style.position = "relative";
+  imgObj.style.left = "0px";
+}
+function moveRight() {
+  imgObj.style.left = parseInt(imgObj.style.left) + 10 + "px";
+  animate = setTimeout(moveRight, 20);
+}
+
+let button2 = document.getElementById("button2");
+button2.onclick = reloadPage;
+
+function reloadPage() {
+  window.location.reload();
+}
+window.onload = init;
